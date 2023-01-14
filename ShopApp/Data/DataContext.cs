5d7c142dde563f4d0d96c8f9293312d11ccb8886
matheush9 <matheushenr941@gmail.com>
2023-01-14 16:43:00 -0300

    added data context

---
+++ ShopApp/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShopApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) :base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products => Set<Product>();
+    }
+}
