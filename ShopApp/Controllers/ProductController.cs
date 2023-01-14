6ee068b65668fd3d48f33587136cf28c6c6c74a6
matheush9 <matheushenr941@gmail.com>
2023-01-14 15:54:10 -0300

    added product controller

---
+++ ShopApp/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopApp.Models;
+using ShopApp.Services;
+
+namespace ShopApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] 
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService= productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return Ok(await _productService.GetAllProducts());
+        }
+    }
+}
