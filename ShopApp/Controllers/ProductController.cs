26c19327c0a430729ad6da962ba6157d6aa0c97b
matheush9 <matheushenr941@gmail.com>
2023-01-15 12:23:30 -0300

    added ProductController CRUD methods

--- ShopApp/Controllers/ProductController.cs
+++ ShopApp/Controllers/ProductController.cs
@@ -18,5 +18,53 @@ namespace ShopApp.Controllers
         {
             return Ok(await _productService.GetAllProducts());
         }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<Product>> GetProduct([FromRoute] int id)
+        {
+            var product = await _productService.GetProductById(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> AddProduct([FromBody] Product newProduct)
+        {
+            return Ok(await _productService.AddProduct(newProduct));
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<ActionResult<Product>> DeleteProduct([FromRoute] int id)
+        {
+            var product = await _productService.DeleteProduct(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<ActionResult<Product>> UpdateProduct([FromRoute] int id, Product newProduct)
+        {
+            var product = await _productService.UpdateProduct(id, newProduct);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
     }
 }
