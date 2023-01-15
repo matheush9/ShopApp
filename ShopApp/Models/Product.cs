b2848103c72f29f509a0464d69075b62ddf3cd5f
matheush9 <matheushenr941@gmail.com>
2023-01-14 22:15:19 -0300

    specify precision type to decimal columns

--- ShopApp/Models/Product.cs
+++ ShopApp/Models/Product.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ShopApp.Models
 {
@@ -10,8 +12,11 @@ namespace ShopApp.Models
         [Required]
         public string Name { get; set; }    
         public string Description { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
 
+        [Column(TypeName = "decimal(18,2)")]
         [Required]
         public decimal PriceTotal { get; set; }
         public string ImageUrl { get; set; }    
