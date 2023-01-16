b5e0c0fdd6107518e52931b6c01088dad101b6f5
matheush9 <matheushenr941@gmail.com>
2023-01-15 22:23:06 -0300

    added product DTOs

---
+++ ShopApp/Dtos/Product/AddProductRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShopApp.Dtos.Product
+{
+    public class AddProductRequestDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public decimal PriceTotal { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
---
+++ ShopApp/Dtos/Product/GetProductResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ShopApp.Dtos.Product
+{
+    public class GetProductResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public decimal PriceTotal { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
