a91c4d211b85d2c711cced3b4699456dee663e86
matheush9 <matheushenr941@gmail.com>
2023-01-28 21:59:10 -0300

    feat: added item and cart models

---
+++ ShopApp/Models/Cart.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopApp.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; } 
+        public List<Item> Items { get; set; }
+        public Customer Customer { get; set; }
+        public int CustomerId { get; set; }      
+    }
+}
---
+++ ShopApp/Models/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopApp.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public Product Product { get; set; }  
+        public int ProductId { get; set; } 
+        public int Quantity { get; set; }
+    }
+}
