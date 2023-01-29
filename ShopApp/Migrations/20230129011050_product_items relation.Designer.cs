dfef50c1c6febf67ee429106d29e7778ca72845a
matheush9 <matheushenr941@gmail.com>
2023-01-28 22:13:30 -0300

    feat: added product_items migration

--- ShopApp/Migrations/20230129011050_product_items relation.Designer.cs
+++ ShopApp/Migrations/20230129011050_product_items relation.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShopApp.Data;
 
@@ -10,9 +11,11 @@ using ShopApp.Data;
 namespace ShopApp.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230129011050_product_items relation")]
+    partial class productitemsrelation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
