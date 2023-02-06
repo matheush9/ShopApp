ac92e763a66879e44d18489516599f385b610d80
Matheus Henrique <matheushenr941@gmail.com>
2023-02-05 23:57:54 -0300

    feat: item DTOs

---
+++ ShopApp/Dtos/Item/AddItemRequestDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ShopApp.Dtos.Item
+{
+    public class AddItemRequestDto
+    {
+        public int Quantity { get; set; }
+        public decimal PriceTotal { get; set; }
+
+        public int ProductId { get; set; }
+        public int CartId { get; set; }
+        public int OrderId { get; set; }
+    }
+}
---
+++ ShopApp/Dtos/Item/GetItemResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShopApp.Dtos.Item
+{
+    public class GetItemResponseDto
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public decimal PriceTotal { get; set; }
+
+        public int ProductId { get; set; }
+        public int CartId { get; set; }
+        public int OrderId { get; set; }
+    }
+}
