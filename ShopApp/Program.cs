a0de635a964e2f5cd36b488686c62f5c08ea0f57
Matheus Henrique <matheushenr941@gmail.com>
2023-04-01 15:04:07 -0300

    use CORS

--- ShopApp/Program.cs
+++ ShopApp/Program.cs
@@ -47,6 +47,8 @@ app.UseMiddleware<GlobalExceptionMiddleware>();
 
 app.UseHttpsRedirection();
 
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
 app.UseAuthorization();
 
 app.MapControllers();
