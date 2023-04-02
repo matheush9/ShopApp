a27aa5f85a74177032267a05d66ff3318e4c331d
Matheus Henrique <matheushenr941@gmail.com>
2023-04-02 14:36:07 -0300

    added "new CreatedAt fields" migration

--- ShopApp/Migrations/20230402171739_new CreatedAt fields.Designer.cs
+++ ShopApp/Migrations/20230402171739_new CreatedAt fields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShopApp.Data;
 
@@ -11,9 +12,11 @@ using ShopApp.Data;
 namespace ShopApp.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230402171739_new CreatedAt fields")]
+    partial class newCreatedAtfields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
