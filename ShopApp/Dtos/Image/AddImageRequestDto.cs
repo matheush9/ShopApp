25abceb52d2395c4899249d54274542d308daf2d
Matheus Henrique <matheushenr941@gmail.com>
2023-04-09 11:27:30 -0300

    feat: added image DTOs

---
+++ ShopApp/Dtos/Image/AddImageRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ShopApp.Dtos.Image
+{
+    public class AddImageRequestDto
+    {
+        public string Name { get; set; }
+        public int ProductId { get; set; }
+    }
+}
---
+++ ShopApp/Dtos/Image/GetImageResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopApp.Dtos.Image
+{
+    public class GetImageResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SmallImagePath { get; set; }
+        public string LargeImagePath { get; set; }
+        public int ProductId { get; set; }
+    }
+}
