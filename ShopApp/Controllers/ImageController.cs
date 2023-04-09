427fe92a6b20ccedad212453d88e715f7cdaf6f3
Matheus Henrique <matheushenr941@gmail.com>
2023-04-09 11:41:37 -0300

    feat: added image controller

---
+++ ShopApp/Controllers/ImageController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ShopApp.Dtos.Image;
+using ShopApp.Services.ImagesServices;
+
+namespace ShopApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : Controller
+    {
+        private readonly IImageService _imageService;
+
+        public ImageController(IImageService imageService)
+        {
+            _imageService = imageService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] int id)
+        {
+            var image = await _imageService.GetById(id);
+
+            if (image is null)
+                return NotFound(image);
+
+            return Ok(image);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromForm] string json, [FromForm] IFormFile imageFile)
+        {
+            Image? newImage = JsonConvert.DeserializeObject<Image>(json);
+
+            if (newImage is null)
+                return BadRequest(newImage);
+
+            if (newImage.ProductId is null)
+            {
+                return BadRequest("The image must have a product associated.");
+            }
+
+            return Ok(await _imageService.Add(newImage, imageFile));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            var image = await _imageService.Delete(id);
+
+            if (image is null)
+                return NotFound(image);
+
+            return Ok(image);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id, AddImageRequestDto newImage)
+        {
+            var image = await _imageService.Update(id, newImage);
+
+            if (image is null)
+                return NotFound(image);
+
+            return Ok(image);
+        }
+    }
+}
