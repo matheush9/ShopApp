05d1b19c84036c736e453c96186ef422a68ada8a
Matheus Henrique <matheushenr941@gmail.com>
2023-04-09 11:31:29 -0300

    feat: image mapper profile

---
+++ ShopApp/Mapper/ImageMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ShopApp.Dtos.Image;
+
+namespace ShopApp.Mapper
+{
+    public class ImageMapperProfile: Profile
+    {
+        public ImageMapperProfile()
+        {
+            CreateMap<Image, GetImageResponseDto>();
+            CreateMap<Image, AddImageRequestDto>();
+            CreateMap<AddImageRequestDto, Image>();
+        }
+    }
+}
