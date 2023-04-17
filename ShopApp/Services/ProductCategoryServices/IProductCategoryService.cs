43dfb3494114cd40813b3c360506ddd71da93cfd
Matheus Henrique <matheushenr941@gmail.com>
2023-04-17 00:09:52 -0300

    feat: product category service/interface

---
+++ ShopApp/Services/ProductCategoryServices/IProductCategoryService.cs
@@ -0,0 +1,9 @@
+﻿using ShopApp.Dtos.Category;
+
+namespace ShopApp.Services.CategoryServices
+{
+    public interface IProductCategoryService
+    {
+        Task<GetProductCategoryResponseDto> GetProductCategoryById(int id);
+    }
+}
