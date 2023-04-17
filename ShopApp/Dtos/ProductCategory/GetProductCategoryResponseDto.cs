272f7c23a2a3443dd10a85d6cf6429ae458d3c00
Matheus Henrique <matheushenr941@gmail.com>
2023-04-17 00:09:38 -0300

    feat: product category dto/mapper

---
+++ ShopApp/Dtos/ProductCategory/GetProductCategoryResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopApp.Dtos.Category
+{
+    public class GetProductCategoryResponseDto
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ ShopApp/Mapper/ProductCategoryMapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using ShopApp.Dtos.Category;
+
+namespace ShopApp.Mapper
+{
+    public class ProductCategoryMapperProfile : Profile
+    {
+        public ProductCategoryMapperProfile()
+        {
+            CreateMap<ProductCategory, GetProductCategoryResponseDto>();
+        }
+    }
+}
