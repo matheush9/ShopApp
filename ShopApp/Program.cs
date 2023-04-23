a4662671e1394a8ee9fb97a06e128fcb8a2d573c
Matheus Henrique <matheushenr941@gmail.com>
2023-04-22 21:54:55 -0300

    feat: added authentication service config
    the default jwt bearer auth configuration

--- ShopApp/Program.cs
+++ ShopApp/Program.cs
@@ -1,7 +1,10 @@
 global using ShopApp.Models;
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using ShopApp.CustomExceptionMiddleware;
 using ShopApp.Data;
 using ShopApp.Dtos.Customer;
@@ -15,6 +18,7 @@ using ShopApp.Services.ImagesServices;
 using ShopApp.Services.ImagesServices.ImageUploadService;
 using ShopApp.Services.ItemServices;
 using ShopApp.Services.OrderServices;
+using ShopApp.Services.PrivateKey;
 using ShopApp.Services.ProductServices;
 using ShopApp.Services.StockServices;
 using ShopApp.Services.StoreServices;
@@ -27,6 +31,22 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = true;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(PrivateKeyService.privateKey),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
 
 builder.Services.AddScoped<IGenericService<GetProductResponseDto, AddProductRequestDto>, ProductService>();
 builder.Services.AddScoped<IGenericService<GetStoreResponseDto, AddStoreRequestDto>, StoreService>();
