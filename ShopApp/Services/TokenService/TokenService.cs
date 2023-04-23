b4233a95227ce0db6f1af0444111c4eb2b448501
Matheus Henrique <matheushenr941@gmail.com>
2023-04-22 21:49:54 -0300

    feat: added JWT token generator service

---
+++ ShopApp/Services/TokenService/TokenService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using ShopApp.Services.PrivateKey;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace ShopApp.Services.TokenService
+{
+    public class TokenService
+    {
+        public static string GenerateToken()
+        {
+            var tokenConfig = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+
+                }),
+
+                Expires = DateTime.UtcNow.AddHours(3),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(PrivateKeyService.privateKey),SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenConfig);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
