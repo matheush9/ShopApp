0cfd6b96e85697ee324fde9d3213b23ebfbae48a
Matheus Henrique <matheushenr941@gmail.com>
2023-04-22 21:48:25 -0300

    feat: added private key service
    it generates a private key with RSA

---
+++ ShopApp/Services/PrivateKey/PrivateKeyService.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+
+namespace ShopApp.Services.PrivateKey
+{
+    public static class PrivateKeyService
+    {
+        public readonly static byte[] privateKey = GeneratePrivateKey();
+
+        private static byte[] GeneratePrivateKey()
+        {
+            var rsa = new RSACryptoServiceProvider(2048);
+            return rsa.ExportRSAPrivateKey();
+        }
+    }
+}
