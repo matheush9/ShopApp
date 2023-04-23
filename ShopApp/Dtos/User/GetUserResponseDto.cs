01df5e8840f62e6d09ed90f180a13ff405ed7a7c
Matheus Henrique <matheushenr941@gmail.com>
2023-04-22 23:17:50 -0300

    feat: added user DTOs

---
+++ ShopApp/Dtos/User/GetUserResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopApp.Dtos.User
+{
+    public class GetUserResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
