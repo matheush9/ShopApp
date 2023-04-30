f57070e9e292af90627b8cfa891e502b83b79fc0
Matheus Henrique <matheushenr941@gmail.com>
2023-04-30 10:13:47 -0300

    feat: added base wrapper response

---
+++ ShopApp.Application/ResponseWrappers/BaseResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace ShopApp.Application.ResponseWrappers
+{
+    public class BaseResponse<T>
+    {
+        public BaseResponse()
+        {
+        }
+        public BaseResponse(T data)
+        {
+            Succeeded = true;
+            Message = string.Empty;
+            Errors = null;
+            Data = data;
+        }
+        public T Data { get; set; }
+        public bool Succeeded { get; set; }
+        public string[] Errors { get; set; }
+        public string Message { get; set; }
+    }
+}
