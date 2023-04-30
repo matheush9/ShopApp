cbcb95a1776a5b67d4cb6a63d9a9044fdcedfceb
Matheus Henrique <matheushenr941@gmail.com>
2023-04-30 10:14:20 -0300

    feat: added paged wrapper response

---
+++ ShopApp.Application/ResponseWrappers/PagedResponse.cs
@@ -0,0 +1,22 @@
+﻿namespace ShopApp.Application.ResponseWrappers
+{
+    public class PagedResponse<T>: BaseResponse<T>
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalRecords { get; set; }
+
+        public PagedResponse(T data, int pageNumber, int pageSize, int totalRecords)
+        {
+            this.PageNumber = pageNumber;
+            this.PageSize = pageSize;
+            this.Data = data;
+            this.Message = null;
+            this.Succeeded = true;
+            this.Errors = null;
+            this.TotalPages = (int)(totalRecords / (double)pageSize);
+            this.TotalRecords = totalRecords;
+        }
+    }
+}
