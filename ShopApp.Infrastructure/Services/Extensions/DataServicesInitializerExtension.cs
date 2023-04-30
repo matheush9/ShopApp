5024668a289246bf4d2268bb45fb3ba90a61e99f
Matheus Henrique <matheushenr941@gmail.com>
2023-04-29 23:31:00 -0300

    feat: added data services initializer extension

---
+++ ShopApp.Infrastructure/Services/Extensions/DataServicesInitializerExtension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ShopApp.Infrastructure.Data;
+
+namespace ShopApp.Infrastructure.Services.Extensions
+{
+    public static partial class DataServicesInitializerExtension
+    {
+        public static IServiceCollection RegisterDataServices(
+          this IServiceCollection services, IConfiguration configuration)
+        {
+            RegisterDataContext(services, configuration);
+            return services;
+        }
+
+        public static void RegisterDataContext(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DataContext>(options =>
+              options.UseSqlServer(configuration.GetConnectionString("ShopConnectionString")));
+        }
+    }
+}
