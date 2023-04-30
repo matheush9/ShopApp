38e3c80586f1b8579c0194d18a1ae771b07d404e
Matheus Henrique <matheushenr941@gmail.com>
2023-04-29 23:31:38 -0300

    refactor: update Program.cs

--- ShopApp/Program.cs
+++ ShopApp/Program.cs
@@ -1,93 +1,14 @@
-global using ShopApp.Models;
-
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using ShopApp.CustomExceptionMiddleware;
-using ShopApp.Data;
-using ShopApp.Dtos.Customer;
-using ShopApp.Dtos.Item;
-using ShopApp.Dtos.Order;
-using ShopApp.Dtos.Products;
-using ShopApp.Dtos.Store;
-using ShopApp.Services.CustomerService;
-using ShopApp.Services.Generic;
-using ShopApp.Services.ImagesService;
-using ShopApp.Services.ImagesServices.ImageUploadService;
-using ShopApp.Services.ItemService;
-using ShopApp.Services.OrderService;
-using ShopApp.Services.PrivateKeyService;
-using ShopApp.Services.ProductService;
-using ShopApp.Services.StockService;
-using ShopApp.Services.StoreService;
-using ShopApp.Services.UserService;
+using ShopApp.Application.Services.Extensions;
+using ShopApp.Infrastructure.CustomMiddlewares.CustomExceptionMiddleware;
+using ShopApp.Infrastructure.Services.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<DataContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ShopConnectionString")));
+builder.Services.RegisterDataServices(builder.Configuration);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddAutoMapper(typeof(Program).Assembly);
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer"
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
-    {
-    {
-        new OpenApiSecurityScheme
-        {
-        Reference = new OpenApiReference
-            {
-            Type = ReferenceType.SecurityScheme,
-            Id = "Bearer"
-            },
-            Scheme = "oauth2",
-            Name = "Bearer",
-            In = ParameterLocation.Header,
-
-        },
-        new List<string>()
-        }
-    });
-});
-
-builder.Services.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(x =>
-{
-    x.RequireHttpsMetadata = true;
-    x.SaveToken = true;
-    x.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(PrivateKeyService.privateKey),
-        ValidateIssuer = false,
-        ValidateAudience = false
-    };
-});
-
-builder.Services.AddScoped<IGenericService<GetProductResponseDto, AddProductRequestDto>, ProductService>();
-builder.Services.AddScoped<IGenericService<GetStoreResponseDto, AddStoreRequestDto>, StoreService>();
-builder.Services.AddScoped<IGenericService<GetItemResponseDto, AddItemRequestDto>, ItemService>();
-builder.Services.AddScoped<IGenericService<GetCustomerResponseDto, AddCustomerRequestDto>, CustomerService>();
-builder.Services.AddScoped<IGenericService<GetOrderResponseDto, AddOrderRequestDto>, OrderService>();
-builder.Services.AddScoped<IStockService, StockService>();
-builder.Services.AddScoped<IProductService, ProductService>();
-builder.Services.AddScoped<IImageUploadService, ImageUploadService>();
-builder.Services.AddScoped<IImageService, ImageService>();
-builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.RegisterApplicationServices();
 
 var app = builder.Build();
 
