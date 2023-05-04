afce20257465f3c5421bd433a38ea0c3ad9c7803
Matheus Henrique <matheushenr941@gmail.com>
2023-05-03 22:46:09 -0300

    feat: product category controller

---
+++ ShopApp/Controllers/ProductCategoryController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopApp.Application.Interfaces.ProductCategory;
+
+namespace ShopApp.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductCategoryController : ControllerBase
+    {
+        private readonly IProductCategoryService _productCategoryService;
+
+        public ProductCategoryController(IProductCategoryService productCategoryService)
+        {
+            _productCategoryService = productCategoryService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetProductCategoryById([FromRoute] int id)
+        {
+            var productCategory = await _productCategoryService.GetProductCategoryById(id);
+
+            if (productCategory is null)
+                return NotFound(productCategory);
+
+            return Ok(productCategory);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllProductCategories()
+        {
+            var categories = await _productCategoryService.GetAllProductCategories();
+
+            if (categories is null)
+                return NotFound(categories);
+
+            return Ok(categories);
+        }
+    }
+}
