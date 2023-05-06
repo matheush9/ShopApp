b9a25237090d4925483e5fadc62d8e75cd07652f
Matheus Henrique <matheushenr941@gmail.com>
2023-05-05 23:53:44 -0300

    feat: added product catalog count migration

--- ShopApp.Infrastructure/Migrations/20230506010428_product catalog count.Designer.cs
+++ ShopApp.Infrastructure/Migrations/20230506010428_product catalog count.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShopApp.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using ShopApp.Infrastructure.Data;
 namespace ShopApp.Infrastructure.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230506010428_product catalog count")]
+    partial class productcatalogcount
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ShopApp.Infrastructure/Migrations/20230506010428_product catalog count.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class productcatalogcount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProductCatalogCount",
+                table: "Stores",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductCatalogCount",
+                table: "Stores");
+        }
+    }
+}
