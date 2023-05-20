ffae047a7ee8e837cb7745c9de977f8c84ee49e3
Matheus Henrique <matheushenr941@gmail.com>
2023-05-20 13:06:40 -0300

    show swagger in production too

--- ShopApp/Program.cs
+++ ShopApp/Program.cs
@@ -12,11 +12,8 @@ builder.Services.RegisterApplicationServices();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseMiddleware<GlobalExceptionMiddleware>();
 
