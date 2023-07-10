4be871620ad026ad330595659472573b18e503db
Matheus Henrique <matheushenr941@gmail.com>
2023-07-09 22:02:50 -0300

    feat: added base repository class and interface

---
+++ ShopApp.Infrastructure/Repositories/Abstractions/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using ShopApp.Domain.Common;
+
+namespace ShopApp.Infrastructure.Repositories.Abstractions
+{
+    public interface IRepository<TEntity>   
+    {
+        Task<TEntity> GetByIdAsync(int id);
+        Task<TEntity> AddAsync(TEntity entity);
+        Task DeleteAsync(TEntity entity);
+        Task<TEntity> UpdateAsync(TEntity entity);
+    }
+}
---
+++ ShopApp.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopApp.Infrastructure.Data;
+using ShopApp.Infrastructure.Repositories.Abstractions;
+
+namespace ShopApp.Infrastructure.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly DataContext _context;
+        protected readonly DbSet<TEntity> _dbset;
+
+        public Repository(DataContext context)
+        {
+            _context = context;
+            _dbset = context.Set<TEntity>();
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            return await _dbset.FindAsync(id);
+        }
+
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            await _dbset.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(TEntity entity)
+        {
+            _dbset.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            _dbset.Update(entity);
+            await _context.SaveChangesAsync();
+            return entity;  
+        }
+    }
+}
